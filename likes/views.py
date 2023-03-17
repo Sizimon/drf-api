00568d4071cdfdf260a95ee6ebc525cf2500db9b
Sizimon <siizmonsus@gmail.com>
2023-03-17 23:00:13 +0000

    Emergancy pause

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializer import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
