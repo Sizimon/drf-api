ca988bdd71a523cd40f3c910c7c55fe2f2ac99a8
Sizimon <siizmonsus@gmail.com>
2023-03-27 21:12:48 +0000

    Add post search (bug with owner_username search)

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,13 +3,14 @@ from posts.models import Post
 from likes.models import Like
 
 
-
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -44,5 +45,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'like_id'
+            'title', 'content', 'image', 'image_filter', 'like_id',
+            'comments_count', 'likes_count',
         ]
